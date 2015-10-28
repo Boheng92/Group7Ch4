0d50a5763410fa88a9cbedc43e974b4e50ca4f66
Nirmit Shah <nirmit2810@gmail.com>
2015-10-27 20:49:30 -0400

    LIDAR SENSOR TEST

--- crawler_withoutPID/crawler_withoutPID.ino
+++ crawler_withoutPID/crawler_withoutPID.ino
@@ -1,17 +1,26 @@
 #include <Servo.h>
-#include <PID_v1.h>
 #include <SoftwareSerial.h>
+//LIDAR SENSOR
+/**********************************************/
+#include <Wire.h>
+#include <I2C.h>
 
+#define    LIDARLite_ADDRESS   0x62          // Default I2C Address of LIDAR-Lite.
+#define    RegisterMeasure     0x00          // Register to write to initiate ranging.
+#define    MeasureValue        0x04          // Value to initiate ranging.
+#define    RegisterHighLowB    0x8f          // Register to get both High and Low bytes in 1 call.
+
+int pos = 0;         // Position of the servo (degress, [0, 180])
+int distance = 0;    // Distance measured
+int sensorPins[] = {2,3}; // Array of pins connected to the sensor Power Enable lines
+int sensorPinsArraySize = 2; // The length of the array
+
+/********************************************/
+//LIDAR SENSOR END
 //create an xBee object
 SoftwareSerial xbee(2,3); // Rx, Tx
 
 
-double Setpoint, Input, Output;
-double Kp=3.0, Ki=0.01, Kd=1.2;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-
-
- 
 Servo wheels; // servo for turning the wheels
 Servo esc; // not actually a servo, but controlled like one!
 bool startup = true; // used to ensure startup only happens once
@@ -20,18 +29,13 @@ double maxSpeedOffset = 45; // maximum speed magnitude, in servo 'degrees'
 double maxWheelOffset = 85; // maximum wheel turn magnitude, in servo 'degrees'
 
 double wheelOffset = 0.0; // For Adjusting the wheel
-double threshHoldDistance = 27.5;
-
-int pin_head = 0;
-int pin_tail = 3;
+double threshHoldDistance = 69.85;
 
 double steerAngle = 0.0;
 double car_length = 13;
 bool goForward = true;
 //double wheelStartUpOffset = 0.0; // For Adjusting the steering
 
-
-int count = 3;
 double distance_sum = 0.0;
 double head_sum = 0.0;
 double tail_sum = 0.0;
@@ -45,36 +49,92 @@ void setup()
    */
   calibrateESC();
 
-    //initialize the variables we're linked to
-  Input = getHeadDis() - getTailDis();
-  Setpoint = 0.1;
-
-  //turn the PID on
-  myPID.SetOutputLimits(-1,1);
-  myPID.SetMode(AUTOMATIC);
-
-
   setVelocity(0.0);
   xbee.begin(9600);
   Serial.begin(9600);
-}
 
-double getHeadDis() {
+  Wire.begin(); // join i2c bus
+  for (int i = 0; i < sensorPinsArraySize; i++){
+    pinMode(sensorPins[i], OUTPUT); // Pin to first LIDAR-Lite Power Enable line
+    Serial.print(sensorPins[i]);
+  }
+}
+int lidarGetRange(void)
+{
+  int val = -1;
+  
+  Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
+  Wire.write((int)RegisterMeasure); // sets register pointer to  (0x00)  
+  Wire.write((int)MeasureValue); // sets register pointer to  (0x00)  
+  Wire.endTransmission(); // stop transmitting
+
+  delay(15); // Wait 20ms for transmit
+
+  Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
+  Wire.write((int)RegisterHighLowB); // sets register pointer to (0x8f)
+  Wire.endTransmission(); // stop transmitting
+
+  delay(15); // Wait 20ms for transmit
+  
+  Wire.requestFrom((int)LIDARLite_ADDRESS, 2); // request 2 bytes from LIDAR-Lite
+
+  if(2 <= Wire.available()) // if two bytes were received
+  {
+    val = Wire.read(); // receive high byte (overwrites previous reading)
+    val = val << 8; // shift high byte to be high 8 bits
+    val |= Wire.read(); // receive low byte as lower 8 bits
+  }
+  return val;
+}
+/*double getHeadDis() {
   double A1 = (double)analogRead(pin_head);
   double distance_head = exp(8.5841-log(A1));
 //  Serial.println("head: "+ (String)distance_head);
   return distance_head;
+}*/
+
+void enableDisableSensor(int sensorPin){
+  for (int i = 0; i < sensorPinsArraySize; i++){
+      digitalWrite(sensorPins[i], LOW); // Turn off all sensors
+  }
+  digitalWrite(sensorPin, HIGH); // Turn on the selected sensor
+  delay(1); // The sensor takes 1msec to wake
 }
 
+double getHeadDis() {
+  enableDisableSensor(2);  
+    int sum = 0; // Variable to store sum
+    for(int i = 0; i < 2; i++){ 
+        int val =  lidarGetRange();
+        if(val<0 || val > 400){
+          i--;
+        }
+        else{
+        sum = sum + val;// Add up all of the readings
+        }
+    }
+    sum = sum/2; // Divide the total by the number of readings to get the average
+    return sum;
+   
+}
 double getTailDis() {
-  double A2 = (double)analogRead(pin_tail);
-  double distance_tail = exp(8.5841-log(A2));
-//  Serial.println("tail: "+ (String)distance_tail);
-  return distance_tail;
+  enableDisableSensor(3);  
+      int sum = 0; // Variable to store sum
+      for(int i = 0; i < 2; i++){ 
+          int val =  lidarGetRange();
+          if(val<0 || val > 400){
+            i--;
+          }
+          else{
+          sum = sum + val;// Add up all of the readings
+          }
+      }
+      sum = sum/2; // Divide the total by the number of readings to get the average
+      return sum;
 }
 
 boolean compareHeadTail(double head, double tail) {
-    if (abs(head-tail) < 2) {
+    if (abs(head-tail) < 5) {
       if (head < 0.7 * threshHoldDistance || head > 1.3 * threshHoldDistance) {
         return false;
       } 
@@ -166,7 +226,6 @@ void steerRight(double d)
 */
 void setVelocity(double s)
 {
-  if( (s >= -1.0 ) && (s <= 1.0))
   {
     esc.write(90 - (s * maxSpeedOffset));
   }
@@ -192,31 +251,24 @@ void loop()
       setVelocity(0.0);
     }
   } else {
-   if (count > 0) {
-      double head_dis = getHeadDis();
-      double tail_dis = getTailDis();
-      if (compareHeadTail(head_dis, tail_dis)) {      
+      head_sum = getHeadDis();
+      tail_sum = getTailDis();
+      if (compareHeadTail(head_sum, tail_sum)) {      
           steerRight(0.1);
       }
-      else {
-        count--;
-        head_sum += getHeadDis();
-        tail_sum += getTailDis(); 
-      }
-   } 
-   else {
-      head_sum /= 3;
-      tail_sum /= 3;
       Serial.println("head:" + (String)head_sum + "   tail:   "+ (String)tail_sum);
+
+      head_sum = getHeadDis();
+      tail_sum = getTailDis();
       if (!compareHeadTail(head_sum, tail_sum)) {
-        steerTheCar(calcDistance(head_sum, tail_sum));  
+         steerTheCar(calcDistance(head_sum, tail_sum));  
       }
       head_sum = 0;
       tail_sum = 0;
-      count = 3;
+ 
    }
-  }
-  delay(100);
+  
+  delay(5);
 }
 
 
